2d3f8a95883325db98464d1d9f1483e865b926c6
daniel.rojas <crimsondex@yahoo.com>
2016-03-15 16:45:09 -0700

    last commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :recipes
+  resources :ingredients, except: [:new, :create]
+  resources :recipes do
+    resources :ingredients, only: [:new, :create, :update]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'recipes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
